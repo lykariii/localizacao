0ca63f86d7cf5e12661125950ad6d9b2b090372c
lykariii <karendecleva3@gmail.com>
2023-11-20 13:52:09 -0400

    loc

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//registrando a service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+      try {
+        let reg;
+        reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+  
+        console.log('Service worker registrada! 😎', reg);
+      } catch (err) {
+        console.log('😥 Service worker registro falhou: ', err);
+      }
+    });
+  }
+
+let posicaoInicial; //variavel para capturar a posicao 
+const capturarLocalizacao = document.getElementById('localizacao');
+const latitude = document.getElementById('latitude');
+const longitude = document.getElementById('longitude');
+const sucesso = (posicao) => { //callback de sucesso para captura da posicao 
+    posicaoInicial = posicao;
+    latitude.innerHTML = posicaoInicial.coords.latitude;
+    longitude.innerHTML = posicaoInicial.coords.longitude;
+};
+
+const erro = (error) => {
+    let errorMessage;
+    switch(error.code){
+    case 0:
+    errorMessage = "Erro desconhecido"
+    break;
+    case 1:
+    errorMessage = "Permissão negada!"
+    break;
+    case 2:
+    errorMessage = "Captura de posição indisponível!"
+    break;
+    case 3:
+    errorMessage = "Tempo de solicitação excedido!"
+    break;
+    }
+    console.log('Ocorreu um erro:' + errorMessage);
+};
+    capturarLocalizacao.addEventListener('click', () => {
+    navigator.geolocation.getCurrentPosition(sucesso, erro);
+});
